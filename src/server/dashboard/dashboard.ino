058974702222246a2867359e5f84eec0257cda10
TheBossMagnus <TheBossMagnus@proton.me>
2025-04-14 09:38:10 +0200

    Merge branch 'master' of https://github.com/TheBossMagnus/antiLupen
    Signed-off-by: TheBossMagnus <TheBossMagnus@proton.me>

--- src/server/dashboard/dashboard.ino
+++ src/server/dashboard/dashboard.ino
@@ -1,42 +1,31 @@
-<<<<<<< HEAD
-const int pinT=3;
-const int pinF=2;
 
-=======
-//const int ledPins[3][3] = {
-//    {3, 4, 5}, // LED  0
-//    {6, 7, 8}, // LED  1
-//    {9, 10, 11} // LED  2
-//};
-
-const int ledPins[2][3] = {
-    {3, 5, 6}, // LED 0
-    {9, 10, 11} // LED 1
-};
->>>>>>> b375a65aca514e9a8745b2291d13484c208378cf
+const int pinT = 3;
+const int pinF = 2;
 
 void setup() {
   Serial.begin(9600);
   pinMode(pinT, OUTPUT);
-pinMode(pinF, OUTPUT);
+  pinMode(pinF, OUTPUT);
+  Serial.print("verde");
+  digitalWrite(pinT, HIGH);
+  digitalWrite(pinF, LOW);
 }
 
 
 void loop() {
-  delay(999);
-    if(Serial.available() > 0) { // Check if data is available
-        String input = Serial.readStringUntil('\n'); // Read string until newline        
-        if(input=="T"){
-          Serial.print("verde");
-          digitalWrite(pinT, HIGH);
-          digitalWrite(pinF, LOW);
-        }
-        else{
-          Serial.print("Rosso");
-                    digitalWrite(pinF, HIGH);
-          digitalWrite(pinT, LOW);
-          
-        }
-      
+  if (Serial.available() > 0) {
+    String input = Serial.readStringUntil('\n');
+    if (input == "T") {
+      Serial.print("verde");
+      digitalWrite(pinT, HIGH);
+      digitalWrite(pinF, LOW);
     }
+    else {
+      Serial.print("Rosso");
+      digitalWrite(pinF, HIGH);
+      digitalWrite(pinT, LOW);
+
+    }
+
+  }
 }
